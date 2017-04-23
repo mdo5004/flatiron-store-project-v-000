0de7b8ec048d7e1ef2b6216d560fca05318e6212
Michael OConnell <michaeldavidoconnell@gmail.com>
2017-04-23 08:49:34 -0400

    Devise installed, migrations set up

--- db/migrate/20140805183007_create_carts.rb
+++ db/migrate/20140805183007_create_carts.rb
@@ -1,5 +1,8 @@
 class CreateCarts < ActiveRecord::Migration
-  def change
-
-  end
+    def change
+        create_table :carts do |t|
+            t.integer :user_id
+            t.boolean :submitted, :default => false
+        end
+    end
 end
--- db/migrate/20140805183012_create_items.rb
+++ db/migrate/20140805183012_create_items.rb
@@ -1,5 +1,9 @@
 class CreateItems < ActiveRecord::Migration
-  def change
-
-  end
+    def change
+        create_table :items do |t|
+            t.string :name
+            t.integer :category_id
+            t.integer :inventory
+        end
+    end
 end
