e95966c31eeacb75757d8995fe8bc181fb08fb9c
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-04-24 09:54:42 -0400

    Passing cart spec

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,11 +1,24 @@
+require 'pry'
 class Cart < ActiveRecord::Base
     has_many :line_items
     has_many :items, through: :line_items
     belongs_to :user
 
     def total
-        
+        item_total = 0
+        items.each do |item|
+            item_total += item.price
+        end
+        item_total
     end
-    def add_item
+    def add_item(item_id)
+        if Item.find_by(id:item_id)
+            li = line_items.where(:cart_id => id, :item_id => item_id).first_or_initialize
+            if li.persisted?
+                li.update(quantity: li.quantity+1)
+            end
+
+            li
+        end
     end
 end
